afe70fb68e6c769eafe98e58bc2bca8c3a6ca851
Mairon <maironpassos@outlook.com>
2020-08-31 22:23:53 -0300

    configs and more

--- src/main/java/com/projeto/curso/boot/domain/Cargo.java
+++ src/main/java/com/projeto/curso/boot/domain/Cargo.java
@@ -1,9 +1,12 @@
 package com.projeto.curso.boot.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @SuppressWarnings("serial")
@@ -18,6 +21,9 @@ public class Cargo extends AbstractEntity<Long> {
 	@JoinColumn(name = "id_departamento_fk")
 	private Departamento departamento;
 
+	@OneToMany(mappedBy="cargo")
+	private List<Funcionario> funcionarios;
+	
 	public String getNome() {
 		return nome;
 	}

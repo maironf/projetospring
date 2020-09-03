9f4511987a6f75d3a103fa8a9f6b2290fef00870
Mairon <maironpassos@outlook.com>
2020-09-02 21:01:27 -0300

    new services

--- src/main/java/com/projeto/curso/boot/service/FuncionarioService.java
+++ src/main/java/com/projeto/curso/boot/service/FuncionarioService.java
@@ -1,5 +1,19 @@
 package com.projeto.curso.boot.service;
 
-public interface FuncionarioService {
+import java.util.List;
 
+import com.projeto.curso.boot.domain.Funcionario;
+
+public interface FuncionarioService {
+	
+	void salvar(Funcionario funcionario);
+	
+	void editar(Funcionario funcionario);
+	
+	void excluir(Long id);
+	
+	Funcionario buscarPorId(Long id);
+	
+	List<Funcionario> buscarTodos();
 }
+ 
